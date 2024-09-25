ebf1de0ea8c943865cd440ff681d23ca2da4f28a
siddharth3008 <siddharthraj82201@gmail.com>
2024-09-25 18:55:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// function add(){
+
+//     // if (true) {
+//     // let car={
+//     //     name:"audi",
+//     //     color:"black",
+//     //     model:100,
+//     //     "car model":"200cc",
+//     //     a:null,
+        
+
+//     // };
+//     //     console.log(car.color);
+//     //     console.log(car["color"])
+//     //     console.log(car["car model"])
+//     //     console.log(typeof car.model);
+//     //     console.log(typeof car.a);
+//     // }
+    
+// }
+// add();
+// var a=["realme","nokia","macbook"];
+// console.log(a[2]);
+
+// var a=10;
+// var b=100;
+// var c=15;
+// var d=15;
+// console.log(c===d);
+// if(b>=a)
+// {
+//     console.log("B")
+// }
+// else{
+//     console.log("A")
+// }
+// console.log(b!=a);
+
+// (a==b)? console.log("equal"):console.log("not equal");
+//function of add//
+// function addition(a,b)
+// {
+//     sum=0;
+//  for(var i=a;i<=b;i++)
+//  {
+//     sum=sum+i;
+//  }
+//  return sum;
+// }
+// let resul=addition(0,5)
+// console.log(result)
+
+const mul=(a,b,c)=>a*b*c;
+console.log(mul(2,4,5)) ;  //aerrow function
